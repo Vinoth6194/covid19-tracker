2b3f8075459a502b0a05ce227b814fdcf34614a6
vin-unicity <vinoth.kumar@unicity.com>
2020-09-19 03:39:50 +0530

    dropdown manual

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
   return (
     <div className="app">
-      <h1>Covid-19 Tracker by Vinothkumar S</h1>
-      {/* <header></header> */}
-      {/* <title , select dropdown> */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        {/* <header></header> */}
+        {/* <title , select dropdown> */}
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* infoboxes */}
       {/* infoboxes */}
       {/* infoboxes */}
