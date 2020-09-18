b686054fda7645f42d9dfe9e130d39bc8506c16b
vin-unicity <vinoth.kumar@unicity.com>
2020-09-19 04:07:14 +0530

    countries looped from api in dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { FormControl, MenuItem, Select } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
@@ -13,7 +28,7 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
